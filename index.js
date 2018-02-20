9503b00b060bfb77f3b0d59b921c84c77d73cf42
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-20 22:10:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,10 +67,10 @@ function removeFromCart(item) {
     var What = cartLiteral.indexOf(item)
     cart.splice(What, [1])
     cartLiteral.splice(What, [1])
-    return cart
+    console.log(cart)
   }
   else {
-    return "That item is not in your cart."
+    console.log("That item is not in your cart.")
   }
 }
 
