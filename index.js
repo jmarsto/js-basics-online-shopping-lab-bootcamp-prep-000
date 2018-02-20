8633ba831e990fe3f5c8f2477c91cdfeea9c6b55
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-20 22:04:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,9 +80,9 @@ function placeOrder(cardNumber) {
   if (cardNumber) {
   var returnOrder = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   cart = []
-  return returnOrder
+  console.log(returnOrder)
   }
   else {
-    return "Sorry, we don't have a credit card on file for you."
+    console.log("Sorry, we don't have a credit card on file for you.")
   }
 }
