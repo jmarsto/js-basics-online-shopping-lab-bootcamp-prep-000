c2cc0a6b463737c3c8699c4545b54cfd44441772
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-20 21:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -77,8 +77,12 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
+  if (cardNumber) {
   var returnOrder = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   cart = []
   return returnOrder
-  
+  }
+  else {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
